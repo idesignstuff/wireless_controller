ece711b97a4647f1ce54706ecd311b6152da6969
idesignstuff <idesignstuff@users.noreply.github.com>
2016-09-20 10:21:22 -0500

    Create xy-TX.ino

---
+++ xy-TX.ino
@@ -0,0 +1,26 @@
+/*
+This is the transmitter file, with x/y proportional control, x indicator lights
+and x function buttons.
+*/
+
+// begin configs
+
+//end configs
+
+
+//begin setup
+
+//end setp
+
+
+//begin loop
+
+//end loop
+
+
+//begin functions
+
+//end functions
+
+
+//end file
